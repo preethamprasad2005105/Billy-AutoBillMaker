702cd64575acce09c78620ca4be0f269c0c547b5
preethamprasad2005105 <150467493+preethamprasad2005105@users.noreply.github.com>
2024-03-17 08:33:38 +0530

    Update menu.py

--- Project/menu.py
+++ Project/menu.py
@@ -1,3 +1,15 @@
 from flet import *
 import flet
 
+class Option(Card):
+  def __init__(self, title : str):
+    super().__init__()
+    self.title = title
+
+class Menu(UserControl):
+  def __init__(self):
+    super().__init__()
+
+  def build(self):
+    return None
+
